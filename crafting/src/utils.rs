071ade0073546559c9ef1ebbc3054757209392c7
Crafting Finance <81205034+CraftingFinance@users.noreply.github.com>
2022-02-16 12:41:58 +0800

    Update utils.rs

--- crafting/src/utils.rs
+++ crafting/src/utils.rs
@@ -1,15 +1,15 @@
-use near_sdk::{ext_contract, Balance, Gas};
+use near_sdk::{ext_contract, AccountId, Balance, Gas};
 use near_sdk::json_types::U128;
 
 pub const NO_DEPOSIT: Balance = 0;
+pub const ONE_YOCTO: Balance = 1;
 pub const ONE_NEAR: Balance = 10u128.pow(24);
 
-/// hotfix_insuffient_gas_for_mft_resolve_transfer.
-pub const GAS_FOR_RESOLVE_TRANSFER: Gas = 20_000_000_000_000;
-pub const GAS_FOR_FT_TRANSFER_CALL: Gas = 25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER;
+pub const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(20_000_000_000_000);
+pub const GAS_FOR_FT_TRANSFER_CALL: Gas = Gas(25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER.0);
 
 /// Amount of gas for fungible token transfers, increased to 20T to support AS token contracts.
-pub const GAS_FOR_FT_TRANSFER: Gas = 20_000_000_000_000;
+pub const GAS_FOR_FT_TRANSFER: Gas = Gas(20_000_000_000_000);
 
 /// Fee divisor, allowing to provide fee in bps.
 pub const FEE_DIVISOR: u32 = 1_000;
